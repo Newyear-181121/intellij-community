1d14801ba24469fad6ab587d1ed7ba6b088a1f89
New Year <yexinnian@banksteel.com>
2022-07-05 16:20:37 +0800

    feat: 加载状态枚举

--- platform/util/base/src/com/intellij/diagnostic/LoadingState.java
+++ platform/util/base/src/com/intellij/diagnostic/LoadingState.java
@@ -8,33 +8,76 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.function.BiConsumer;
 
+/**
+ * 加载状态
+ */
 @ApiStatus.Internal
 public enum LoadingState {
   BOOTSTRAP("bootstrap"),
+  /**
+   * 基础 LAF 已初始化
+   */
   BASE_LAF_INITIALIZED("LaF is initialized"),
+  /**
+   * 组件已注册
+   */
   COMPONENTS_REGISTERED("app component registered"),
+  /**
+   * 配置存储已初始化
+   */
   CONFIGURATION_STORE_INITIALIZED("app store initialized"),
+  /**
+   * 加载的组件
+   */
   COMPONENTS_LOADED("app component loaded"),
+  /**
+   * 应用启动
+   */
   APP_STARTED("app started"),
+  /**
+   * 项目启动
+   */
   PROJECT_OPENED("project opened");
 
+  /**
+   * 显示名称
+   */
   final String displayName;
 
+  /**
+   * 错误处理程序
+   */
   @SuppressWarnings("StaticNonFinalField")
   public static BiConsumer<String, Throwable> errorHandler;
 
+  /**
+   * 检查加载阶段
+   */
   private static boolean CHECK_LOADING_PHASE;
+  /**
+   * 堆栈跟踪
+   */
   private static Set<ThrowableWrapper> stackTraces;
 
+  /**
+   * 构造方法
+   * @param displayName
+   */
   LoadingState(@NotNull String displayName) {
     this.displayName = displayName;
   }
 
+  /**
+   * 设置严格模式
+   */
   @ApiStatus.Internal
   public static void setStrictMode() {
     CHECK_LOADING_PHASE = true;
   }
 
+  /**
+   * 检查发生
+   */
   public void checkOccurred() {
     if (!CHECK_LOADING_PHASE) {
       return;
@@ -48,6 +91,10 @@ public enum LoadingState {
     logStateError(currentState);
   }
 
+  /**
+   * 记录状态错误
+   * @param currentState
+   */
   private synchronized void logStateError(@NotNull LoadingState currentState) {
     Throwable t = new Throwable();
     if (stackTraces == null) {
@@ -94,6 +141,11 @@ public enum LoadingState {
       return fingerprint(throwable).hashCode();
     }
 
+    /**
+     * 获取异常指纹，及堆栈记录
+     * @param throwable
+     * @return
+     */
     private static String fingerprint(Throwable throwable) {
       StringBuilder sb = new StringBuilder();
       for (StackTraceElement traceElement : throwable.getStackTrace()) {
