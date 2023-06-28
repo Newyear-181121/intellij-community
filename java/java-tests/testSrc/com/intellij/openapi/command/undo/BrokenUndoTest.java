4d00499d0d68bae40aeca80762e157c1990b8f78
NewYear <mail_newyear@163.com>
2023-06-28 18:14:04 +0800

    fix(启动): 测试类启动项目成功

--- java/java-tests/testSrc/com/intellij/openapi/command/undo/BrokenUndoTest.java
+++ java/java-tests/testSrc/com/intellij/openapi/command/undo/BrokenUndoTest.java
@@ -5,13 +5,46 @@ import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.impl.EditorImpl;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.Consumer;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+
 public class BrokenUndoTest extends UndoTestCase {
+
+  public void test_myTestViewer() {
+    System.out.println(myRoot);
+    VirtualFile file = createChildData(myRoot, "f.txt");
+    final Document document = FileDocumentManager.getInstance().getDocument(file);
+    assertNotNull(document);
+    EditorFactory editorFactory = EditorFactory.getInstance();
+    Editor editor = editorFactory.createViewer(document);
+    if (editor instanceof EditorImpl) {
+      EditorImpl impl = (EditorImpl) editor;
+      JFrame jFrame = new JFrame("测试");
+      jFrame.setSize(300, 300);
+      jFrame.setLocation(300, 200);
+      jFrame.add(impl.getScrollPane());
+      jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      jFrame.setVisible(true);
+    }
+  }
+
+  public static void main(String[] args) {
+    BrokenUndoTest test = new BrokenUndoTest();
+
+    test.test_myTestViewer();
+  }
+
+  //public BrokenUndoTest() {
+  //  super.myRoot = createTestProjectStructure();
+  //}
+
   public void testWithEditorViewer() {
+    System.out.println(myRoot);
     VirtualFile file = createChildData(myRoot, "f.txt");
     final Document document = FileDocumentManager.getInstance().getDocument(file);
     assertNotNull(document);
